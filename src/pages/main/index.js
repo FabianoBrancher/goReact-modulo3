face1b00ac41476ad86183ff6f0e50fdfb529de1
Fabiano Brancher <fabianobrancher@gmail.com>
2019-06-10 22:41:30 -0300

    duck pattern

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,7 +3,8 @@ import PropTypes from "prop-types";
 
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import * as FavoriteActions from "../../store/actions/favorites";
+// import * as FavoriteActions from "../../store/actions/favorites";
+import { Creators as FavoriteActions } from '../../store/ducks/favorites'
 
 class Main extends Component {
   static propTypes = {
--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 
-import favorites from "./favorites";
+import favorites from "../ducks/favorites";
 
 export default combineReducers({
   favorites
